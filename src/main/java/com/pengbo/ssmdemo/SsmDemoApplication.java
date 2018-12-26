31e375642b7a56080804b71ce95eb47fc99db205
pengbo <sunshine_pb@qq.com>
2018-12-27 02:07:16 +0800

    2018-12-27 02:06:12

--- src/main/java/com/pengbo/ssmdemo/SsmDemoApplication.java
+++ src/main/java/com/pengbo/ssmdemo/SsmDemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SsmDemoApplication {
 
     public static void main(String[] args) {
+        //启动
         SpringApplication.run(SsmDemoApplication.class, args);
     }
 }
